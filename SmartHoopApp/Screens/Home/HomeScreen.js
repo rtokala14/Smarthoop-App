71f6bc7a3aeeb105b9b2293f489d007b821a04ac
Rohit Tokala <rtokala@purdue.edu>
2022-10-06 20:05:31 -0400

    Customize headers and add screen transitions

--- SmartHoopApp/Screens/Home/HomeScreen.js
+++ SmartHoopApp/Screens/Home/HomeScreen.js
@@ -9,13 +9,47 @@ import Timed from './Stacks/Timed';
 
 const Stack = createStackNavigator();
 
+const config = {
+  animation: 'spring',
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
 export default HomeScreen = ({navigation}) => {
   return (
-    <Stack.Navigator screenOptions={(headerShown = 'false')}>
-      <Stack.Screen name="HomePage" component={HomePage} />
-      <Stack.Screen name="FreeShoot" component={FreeShoot} />
-      <Stack.Screen name="Shootout" component={Shootout} />
-      <Stack.Screen name="Timed" component={Timed} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: '#242020'},
+        headerTitleAlign: 'center',
+        headerTintColor: '#d1341f',
+        // presentation: 'modal',
+        transitionSpec: {
+          open: config,
+          close: config,
+        },
+      }}>
+      <Stack.Screen
+        name="HomePage"
+        options={{title: 'SmartHoop'}}
+        component={HomePage}
+      />
+      <Stack.Screen
+        name="FreeShoot"
+        options={{title: 'FreeShoot'}}
+        component={FreeShoot}
+      />
+      <Stack.Screen
+        name="Shootout"
+        options={{title: 'Shootout'}}
+        component={Shootout}
+      />
+      <Stack.Screen name="Timed" options={{title: 'Timed'}} component={Timed} />
     </Stack.Navigator>
   );
 };
