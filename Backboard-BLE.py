c0ff6d45320516e8b23745a85c88ae135ba1b4fc
rtokala14 <54463676+rtokala14@users.noreply.github.com>
2022-10-21 20:41:34 -0400

    Add micropython code for backboard

---
+++ Backboard-BLE.py
@@ -0,0 +1,71 @@
+from machine import Pin
+from machine import Timer
+from time import sleep_ms
+import ubluetooth
+
+ble_msg = ""
+is_ble_connected = False
+
+class ESP32_BLE():
+    def __init__(self, name):
+        self.name = name
+        self.ble = ubluetooth.BLE()
+        self.ble.active(True)
+        self.disconnected()
+        self.ble.irq(self.ble_irq)
+        self.register()
+        self.advertiser()
+        
+    def connected(self):
+        global is_ble_connected
+        is_ble_connected = True
+    
+    def disconnected(self):
+        global is_ble_connected
+        is_ble_connected = False
+        
+    def ble_irq(self, event, data):
+        global ble_msg
+        
+        if event == 1: #IRQ_CENTRAL_CONNECT
+            self.connected()
+        elif event == 2: #IRQ_CENTRAL_DISCONNECT
+            self.advertiser()
+            self.disconnected()
+        elif event == 3: #IRQ_GATTS_WRITE
+            buffer = self.ble.gatts_read(self.rx)
+            ble_msg = buffer.decode('UTF-8').strip()
+            print(ble_msg)
+    
+    def register(self):        
+        # Nordic UART Service (NUS)
+        SERVICE_UUID = '4fafc201-1fb5-459e-8fcc-c5c9c331914b'
+        SHOT_CHARACTERISTIC_UUID = '6d68efe5-04b6-4a85-abc4-c2670b7bf7fd'
+        TX_UUID = '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'
+            
+        BLE_NUS = ubluetooth.UUID(SERVICE_UUID)
+        BLE_RX = (ubluetooth.UUID(SHOT_CHARACTERISTIC_UUID), ubluetooth.FLAG_NOTIFY)
+        BLE_TX = (ubluetooth.UUID(TX_UUID), ubluetooth.FLAG_NOTIFY)
+            
+        BLE_UART = (BLE_NUS, (BLE_TX, BLE_RX, ))
+        SERVICES = (BLE_UART, )
+        ((self.tx, self.rx,), ) = self.ble.gatts_register_services(SERVICES)
+    
+    def send(self, data):
+        self.ble.gatts_notify(0, self.tx, data + '\n')
+
+    def advertiser(self):
+        name = bytes(self.name, 'UTF-8')
+        adv_data = bytearray('\x02\x01\x02') + bytearray((len(name) + 1, 0x09)) + name
+        self.ble.gap_advertise(100, adv_data)
+        print(adv_data)
+        print("\r\n")
+        
+ble = ESP32_BLE("Backboard-1")
+
+while True:
+    if is_ble_connected:
+        ble.send("Hello")
+        ble.send("Rohit")
+    sleep_ms(1000)
+    
